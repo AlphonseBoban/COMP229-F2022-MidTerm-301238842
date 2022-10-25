144e37e780d27766209bc0c90b9ea309c7295e95
AlphonseBoban <aboban2@my.centennialcollege.ca>
2022-10-25 15:15:24 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,6 @@
 module.exports = {
   //local MongoDB deployment ->
-  URI: "mongodb://127.0.0.1/faculty_info",
+  //URI: "mongodb://127.0.0.1/faculty_info",
+
+  URI: "mongodb+srv://newuser:pZvTMdEehwEXgvzk@mongodbcluster1.ggpzslr.mongodb.net/faculty_info?retryWrites=true&w=majority"
 };
